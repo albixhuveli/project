f24b47632f471687208b740ed32b652d5693a769
albixhuveli <albi.xhuveli.601@gmail.com>
2023-06-25 01:57:02 -0400

    training edit

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main (String[] args) {
+        int[] array = new int[5];
+        array[0] = 155;
+        array[5] = 77;
+        System.out.print(array[5]);
+
+        String[] words = {"abc", "def", "jf"}
+    }
+}
