462a234f80e65752522af01e4d5e40c705ef318c
albixhuveli <albi.xhuveli.601@gmail.com>
2023-07-01 15:46:23 -0400

    practice

--- array.java
+++ array.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class array {
     public static void main (String[] args) {
-        int[] array = new int[5];
+        int[] array = new int[6];
         array[0] = 155;
         array[5] = 77;
         System.out.print(array[5]);
 
         String[] words = {"abc", "def", "jf"};
+        words[0] = "dee";
+        System.out.println(Arrays.toString(words));
     }
 }
